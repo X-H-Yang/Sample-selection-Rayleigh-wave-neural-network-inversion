561c79385f2937bb4f57b9c8901f14f41a88cd16
Xiao-Hui Yang <xiao-hui.yang@hotmail.com>
2024-01-05 12:06:03 +0800

    Update main_sampleSelectionInversion.m

--- main_sampleSelectionInversion.m
+++ main_sampleSelectionInversion.m
@@ -4,9 +4,10 @@
 %
 % paper status: Published - Early access 
 %               (journal: IEEE Transactions on Geoscience and Remote Sensing)
-% Xiao-Hui Yang, Qiang Zu, Yuanyuan Zhou, Peng Han *, Xiaofei Chen (2023). 
+% Xiao-Hui Yang, Qiang Zu, Yuanyuan Zhou, Peng Han *, Xiaofei Chen (2024). 
 % A Sample Selection Method for Neural-network-based Rayleigh Wave Inversion. 
-% IEEE Transactions on Geoscience and Remote Sensing, doi: 10.1109/TGRS.2023.3341955.
+% IEEE Transactions on Geoscience and Remote Sensing, 62, pp. 1-17.
+% doi: 10.1109/TGRS.2023.3341955.
 %
 % software version: MATLAB R2017a
 %
